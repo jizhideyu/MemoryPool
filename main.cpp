9e08b02d21c7ad9e1eef6c75e379b7fa8e50c7cd
yzy <529592364@qq.com>
2017-11-26 13:30:04 +0800

    1126

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <cassert>
 #include <vector>
+#include <ctime>
+#include "StackList.h"
 
-using namespace std;
+#define  ELEMS  25000000
 
 int main()
 {
-	
+	clock_t time_start;
+	stackList<int, std::allocator<int> > test_stack_list;
+	time_start = clock();
+	for (int i = 0; i < ELEMS; ++i)
+	{
+		test_stack_list.push(i);
+		test_stack_list.pop();
+	}
+	std::cout << ((clock() - time_start) / CLOCKS_PER_SEC)<< '\n';
+
+	system("pause");
 }
--- stackList.h
+++ stackList.h
@@ -6,7 +6,7 @@
 template <typename T>
 class stackNode
 {
-private:
+public:
 	T data;
 	stackNode* pre_node;
 };
@@ -15,7 +15,7 @@ template<typename T, typename Alloc>
 class stackList
 {
 public:
-	typedef stackNode node;
+	typedef stackNode<T> node;
 	typedef typename Alloc::template rebind<node>::other allocator;
 
 	stackList();
@@ -24,12 +24,64 @@ public:
 	bool empty();
 	void clear();
 	void push(T element);
-	T pop();
-	T getTop();
+	T pop();	
 
 private:
 	allocator allocator_;
 	node* head_node_;
 };
+
+template <typename T, typename Alloc>
+stackList<T, Alloc>::stackList()
+{
+	head_node_ = nullptr;
+}
+
+template <typename T, typename Alloc>
+stackList<T, Alloc>::~stackList()
+{
+	clear();
+}
+
+template <typename T, typename Alloc>
+void stackList<T, Alloc>::push(T element)
+{
+	node* new_node = allocator_.allocate(1);
+	allocator_.construct(new_node, node());
+	new_node->data = element;
+	new_node->pre_node = head_node_;
+	head_node_ = new_node;
+}
+
+template <typename T, typename Alloc>
+bool stackList<T, Alloc>::empty()
+{
+	return (head_node_ == nullptr);
+}
+
+template <typename T, typename Alloc>
+T stackList<T, Alloc>::pop()
+{
+	T result = head_node_->data;
+	node* temp = head_node_->pre_node;
+	allocator_.destroy(head_node_);
+	allocator_.deallocate(head_node_, 1);
+	head_node_ = temp;
+	return result;
+}
+
+template <typename T, typename Alloc>
+void stackList<T, Alloc>::clear()
+{
+	node* current_node = head_node_;
+	while (current_node)
+	{
+		node* temp = current_node->pre_node;
+		allocator_.destroy(current_node);
+		allocator_.deallocate(current_node, 1);
+		current_node = temp;
+	}
+	head_node_ = nullptr;
+}
 
 #endif
