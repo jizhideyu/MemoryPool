37efabcefa58ce2788e8a11ac363d80dc8825230
yzy <529592364@qq.com>
2017-11-30 21:23:18 +0800

    1130

---
+++ MemoryPool.h
@@ -0,0 +1,22 @@
+#ifndef MEMORYPOOL_H
+#define MEMORYPOOL_H
+
+template<typename T>
+class memoryPool
+{
+public:
+	memoryPool();
+	~memoryPool();
+};
+
+template<typename T>
+memoryPool::memoryPool()
+{
+}
+
+template<typename T>
+memoryPool::~memoryPool()
+{
+}
+
+#endif
