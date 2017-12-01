dcfb24b244131f4c4814833d44614310c8e8d408
yzy <529592364@qq.com>
2017-12-01 22:44:22 +0800

    1201

--- MemoryPool.h
+++ MemoryPool.h
@@ -4,19 +4,61 @@
 template<typename T>
 class memoryPool
 {
+private:
+	typedef char* data_ptr;
+	typedef 
+
 public:
 	memoryPool();
 	~memoryPool();
+
+	T* allocate();
+	void deallocate();
+
+	void construct();
+	void destroy();
+
+	template<typename U>
+	class rebind
+	{
+	public:
+		typedef	memoryPool<U> other;
+	};
 };
 
 template<typename T>
 memoryPool::memoryPool()
 {
+
 }
 
 template<typename T>
 memoryPool::~memoryPool()
 {
+}
+
+template<typename T>
+T* memoryPool::allocate()
+{
+
+}
+
+template<typename T>
+void memoryPool::deallocate()
+{
+
+}
+
+template<typename T>
+void memoryPool::construct()
+{
+
+}
+
+template<typename T>
+void memoryPool::destroy()
+{
+
 }
 
 #endif
