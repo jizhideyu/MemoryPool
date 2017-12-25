63bc27eeab8e9b96a30d0cda656db5384c161e06
yzy <529592364@qq.com>
2017-12-25 21:41:47 +0800

    1225

--- MemoryPool.h
+++ MemoryPool.h
@@ -1,3 +1,6 @@
+#include <climits>
+#include <cstddef>
+
 #ifndef MEMORYPOOL_H
 #define MEMORYPOOL_H
 
@@ -5,7 +8,7 @@
 #define BLOCK_SIZE 4096
 
 template<typename T>
-class memoryPool
+class blockAllocator
 {
 private:
 	union slot
@@ -20,97 +23,77 @@ private:
 	slotPtr m_current_slot;
 	slotPtr m_last_slot;
 	slotPtr m_free_slots;
-
-	assert(BLOCK_SIZE >= 2 * sizeof(slot), "BLOCKSIZE is too small");
+	//assert(BLOCK_SIZE >= 2 * sizeof(slot), "BLOCKSIZE is too small");
 
 public:
-	memoryPool();
-	~memoryPool();
+	blockAllocator()
+	{
+		m_current_block = nullptr;
+		m_current_slot = nullptr;
+		m_last_slot = nullptr;
+		m_free_slots = nullptr;
+	}
+	~blockAllocator()
+	{
+		slotPtr cur_ptr = m_current_block;
+		while (cur_ptr != nullptr)
+		{
+			slotPtr pre_ptr = cur_ptr->next;
+			MP_DELETE(cur_ptr);
+			cur_ptr = pre_ptr;
+		}
+	}
 
-	T* allocate(size_t n = 1);
-	void deallocate(T* ptr);
+	T* allocate(size_t n = 1)
+	{
+		if (m_free_slots)
+		{
+			T* result = reinterpret_cast<T*>(m_free_slots);
+			m_free_slots = m_free_slots->next;
+			return result;
+		}
+		else
+		{
+			if (m_current_slot >= m_last_slot)
+			{
+				dataPtr newBlock = reinterpret_cast<dataPtr>(operator new(BLOCK_SIZE));
+				reinterpret_cast<slotPtr>(newBlock)->next = m_current_block;
+				m_current_block = reinterpret_cast<slotPtr>(newBlock);
+
+				dataPtr body = newBlock + sizeof(slotPtr);
+				uintptr_t result = reinterpret_cast<uintptr_t>(body);
+				size_t body_padding = result - (result / std::alignment_of<slot>::value * std::alignment_of<slot>::value);
+				m_current_slot = reinterpret_cast<slotPtr>(body + body_padding);
+				m_last_slot = reinterpret_cast<slotPtr>(newBlock + BLOCK_SIZE - sizeof(slot));
+			}
+			return reinterpret_cast<T*>(m_current_slot++);
+		}
+	}
+	void deallocate(T* ptr, size_t size = 1)
+	{
+		if (ptr != nullptr)
+		{
+			reinterpret_cast<slotPtr>(ptr)->next = m_free_slots;
+			m_free_slots = reinterpret_cast<slotPtr>(ptr);
+		}
+	}
 
 	template<typename U, typename... Args>
-	void construct(U* p, Args... args);
+	void construct(U* p, Args... args)
+	{
+		new (p)U(std::forward<Args>(args)...);
+	}
 	template<typename U>
-	void destroy(U* p);
+	void destroy(U* p)
+	{
+		p->~U();
+	}
 
 	template<typename U>
 	class rebind
 	{
 	public:
-		typedef	memoryPool<U> other;
+		typedef	blockAllocator<U> other;
 	};
 };
-
-template<typename T>
-memoryPool::memoryPool()
-{
-	m_current_block = nullptr;
-	m_current_slot = nullptr;
-	m_last_slot = nullptr;
-	m_free_slots = nullptr;
-}
-
-template<typename T>
-memoryPool::~memoryPool()
-{
-	slotPtr cur_ptr = m_current_block;
-	while (cur_ptr != nullptr)
-	{
-		slotPtr pre_ptr = cur_ptr->next;
-		MP_DELETE(cur_ptr);
-		cur_ptr = pre_ptr;
-	}
-}
-
-template<typename T>
-T* memoryPool::allocate(size_t n = 1)
-{
-	if (m_free_slots)
-	{
-		T* result = reinterpret_cast<T*>(m_free_slots);
-		m_free_slots = m_free_slots.next;
-		return result;
-	}
-	else
-	{
-		if (m_current_slot >= m_last_slot)
-		{
-			dataPtr newBlock = reinterpret_cast<dataPtr>(operator new(BLOCK_SIZE));
-			reinterpret_cast<slotPtr>(newBlock)->next = m_current_block;
-			m_current_block = reinterpret_cast<slotPtr>(newBlock);
-
-			dataPtr body = newBlock + sizeof(slotPtr);
-			UINT_PTR result = reinterpret_cast<UINT_PTR>(body);
-			size_t body_padding = result - (result / alignof(slot) * alignof(slot));
-			m_current_slot = reinterpret_cast<slotPtr>(body + body_padding);
-			m_last_slot = reinterpret_cast<slotPtr>(newBlock + BLOCK_SIZE - sizeof(slot));
-		}
-		return reinterpret_cast<T*>(m_current_slot++);
-	}
-}
-
-template<typename T>
-void memoryPool::deallocate(T* ptr)
-{
-	if (ptr != nullptr)
-	{
-		reinterpret_cast<slotPtr>(ptr)->next = m_free_slots;
-		m_free_slots = reinterpret_cast<slotPtr>(ptr);
-	}
-}
-
-template<typename U, typename... Args>
-void memoryPool::construct(U* p, Args... args)
-{
-	new (p) U(std::forward<Args>(args)...);
-}
-
-template<typename U>
-void memoryPool::destroy(U* p)
-{
-	p->~U();
-}
-
-#endif
+#endif //MEMORYPOOL_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <cassert>
 #include "StackList.h"
+#include "MemoryPool.h"
 
-#define  ELEMS  25000000
+#define ELEMS  25000000
+#define TEST_TYPE int
 
 int main()
 {
 	clock_t time_start;
-	stackList<int, std::allocator<int> > test_stack_list;
+
+	stackList<TEST_TYPE, std::allocator<TEST_TYPE> > std_test_stack_list;
 	time_start = clock();
 	for (int i = 0; i < ELEMS; ++i)
 	{
-		test_stack_list.push(i);
-		test_stack_list.pop();
+		std_test_stack_list.push(i);
+		std_test_stack_list.pop();
+	}
+	std::cout <<((clock() - time_start) / CLOCKS_PER_SEC) << '\n';
+
+	stackList<TEST_TYPE, blockAllocator<TEST_TYPE> > ba_test_stack_list;
+	time_start = clock();
+	for (int i = 0; i < ELEMS; ++i)
+	{
+		ba_test_stack_list.push(i);
+		ba_test_stack_list.pop();
 	}
 	std::cout << ((clock() - time_start) / CLOCKS_PER_SEC)<< '\n';
 
